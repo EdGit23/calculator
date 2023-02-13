6251d700e82bf48ad241fd979ac0dd454e3696fe
Edwin Wagha <123566890+EdGit23@users.noreply.github.com>
2023-02-13 08:59:41 +0300

    Different Version Update

--- main.py
+++ main.py
@@ -1,90 +1,191 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+import tkinter.ttk as ttk
+from string import punctuation
 
-root = Tk()
+class App(tk.Tk):
+	def __init__(self):
+		super().__init__()
 
-root.title("Basic Calculator")
+		self.title('Calculator')
+		self.geometry('300x400')
+		self.configure(bg='gray')
+		self.resizable(False,False)
+		self.error_message = 'ERROR DIVISION BY ZERO'
+		self.variable_check = False
 
-def button_click(number):
-	current_value = entry_widget.get()
-	entry_widget.delete(0,END)
+		self.frame1 = tk.Frame(self)
+		self.frame1.grid(row=0, column=0, pady=(0,10))
 
-	entry_widget.insert(0,str(current_value) + str(number))
-
-def calculate_method(character):
-	global character_value
-	global number1, number2
-
-	number1 = eval(entry_widget.get())
-
-	entry_widget.delete(0,END)
-	
-	character_value = character
+		self.frame2 = tk.Frame(self)
+		self.frame2.grid(row=1, column=0, pady=(0,10))
 
 
-def equals_method():
-	number2 = eval(entry_widget.get())
-
-	entry_widget.delete(0,END)
-
-	if character_value == '+':
-		total = number1 + number2
-		entry_widget.insert(0,str(total))
-
-	elif character_value == '-':
-		total = number1 - number2
-		entry_widget.insert(0,str(total))
-
-	elif character_value == '*':
-		total = number1 * number2
-		entry_widget.insert(0,str(total))
-
-	elif character_value == '/':
-		total = number1 / number2
-		entry_widget.insert(0,str(total))
-
-def clear_method():
-	entry_widget.delete(0,END)
-
-frame0 = ttk.Frame(root,padding=10)
-frame1 = ttk.Frame(root,padding=1)
-frame2 = ttk.Frame(root,padding=1)
-frame3 = ttk.Frame(root,padding=1)
-frame4 = ttk.Frame(root,padding=1)
-frame5 = ttk.Frame(root,padding=1)
-frame6 = ttk.Frame(root,padding=1)
+		self.frame3 = tk.Frame(self)
+		self.frame3.grid(row=2, column=0, pady=(0,10))
 
 
-frame0.grid()
-frame1.grid()
-frame2.grid()
-frame3.grid()
-frame4.grid()
-frame5.grid()
-frame6.grid()
+		self.frame4 = tk.Frame(self)
+		self.frame4.grid(row=3, column=0, pady=(0,10))
 
 
-entry_widget = ttk.Entry(frame0, width=30)
-entry_widget.grid()
-
-ttk.Button(frame1, text=str(1),padding=12,width=5,command=lambda:button_click(1)).grid(row=0, column=0)
-ttk.Button(frame1, text=str(2),padding=12,width=5,command=lambda:button_click(2)).grid(row=0, column=1)
-ttk.Button(frame1, text=str(3),padding=12,width=5,command=lambda:button_click(3)).grid(row=0, column=2)
-ttk.Button(frame2, text=str(4),padding=12,width=5,command=lambda:button_click(4)).grid(row=1, column=0)
-ttk.Button(frame2, text=str(5),padding=12,width=5,command=lambda:button_click(5)).grid(row=1, column=1)
-ttk.Button(frame2, text=str(6),padding=12,width=5,command=lambda:button_click(6)).grid(row=1, column=2)
-ttk.Button(frame3, text=str(7),padding=12,width=5,command=lambda:button_click(7)).grid(row=2, column=0)
-ttk.Button(frame3, text=str(8),padding=12,width=5,command=lambda:button_click(8)).grid(row=2, column=1)
-ttk.Button(frame3, text=str(9),padding=12,width=5,command=lambda:button_click(9)).grid(row=2, column=2)
-ttk.Button(frame4, text="+",padding=12,width=5, command=lambda:calculate_method('+')).grid(row=0,column=0)
-ttk.Button(frame4, text=str(0),padding=12,width=5,command=lambda:button_click(0)).grid(row=0, column=1)
-ttk.Button(frame4, text="/",padding=12,width=5, command=lambda:calculate_method('/')).grid(row=0,column=2)
-ttk.Button(frame5, text="CLR",padding=12,width=5, command=clear_method).grid(row=0,column=0)
-ttk.Button(frame5, text="-",padding=12,width=5,command=lambda:calculate_method('-')).grid(row=0,column=1)
-ttk.Button(frame5, text="*",padding=12,width=5,command=lambda:calculate_method('*')).grid(row=0,column=2)
-ttk.Button(frame6, text="=", command=equals_method, width =15, padding=12).grid(row=0,column=0)
-ttk.Button(frame6, text=".", command=lambda:button_click('.'), width =5, padding=12).grid(row=0,column=1)
+		self.frame5 = tk.Frame(self)
+		self.frame5.grid(row=4, column=0, pady=(0,10))
 
 
+		self.frame6 = tk.Frame(self)
+		self.frame6.grid(row=5, column=0, pady=(0,10))
 
-root.mainloop()
+
+		self.frame7 = tk.Frame(self)
+		self.frame7.grid(row=6, column=0, pady=(0,10))
+
+
+		self.frame8 = tk.Frame(self)
+		self.frame8.grid(row=7, column=0, pady=(0,10))
+
+		#Entry widget for the calculator
+		self.display_window = tk.Text(self.frame1,bd=4,bg='#a8c64e',fg='darkgreen',font=('Verdana',12), width=25, height=3)
+		self.display_window.grid(row=0, column=0,padx=20,pady=10)
+
+		#frame2
+		self.button1 = ttk.Button(self.frame2,text='sin',width=9,padding=1).grid(row=0, column=0)
+		self.button2 = ttk.Button(self.frame2,text='cos',width=9,padding=1).grid(row=0, column=1)
+		self.button3 = ttk.Button(self.frame2,text='tan',width=9,padding=1).grid(row=0, column=2)
+		self.button4 = ttk.Button(self.frame2,text='sqrt',width=9,padding=1).grid(row=0, column=3)
+
+		#frame3
+		self.button1 = ttk.Button(self.frame3,text='x^2',width=9,padding=1).grid(row=0, column=0)
+		self.button2 = ttk.Button(self.frame3,text='x^3',width=9,padding=1).grid(row=0, column=1)
+		self.button3 = ttk.Button(self.frame3,text='x',width=9,padding=1).grid(row=0, column=2)
+		self.button4 = ttk.Button(self.frame3,text='log',width=9,padding=1).grid(row=0, column=3)
+
+		#frame4
+		self.button1 = ttk.Button(self.frame4,text='DEL',width=9,padding=1, command=self.clear_screen).grid(row=0, column=0)
+		self.button2 = ttk.Button(self.frame4,text='pi',width=9,padding=1, command=lambda:self.constants_method('pi')).grid(row=0, column=1)
+		self.button3 = ttk.Button(self.frame4,text='shift',width=9,padding=1).grid(row=0, column=2)
+		self.button4 = ttk.Button(self.frame4,text='%',width=9,padding=1,command=lambda:self.calculate_method('%')).grid(row=0, column=3)
+
+		#frame5
+		self.button1 = ttk.Button(self.frame5,text='7',width=7,padding=7, command=lambda:self.num_clicks('7')).grid(row=0, column=0)
+		self.button2 = ttk.Button(self.frame5,text='8',width=7,padding=7, command=lambda:self.num_clicks('8')).grid(row=0, column=1)
+		self.button3 = ttk.Button(self.frame5,text='9',width=7,padding=7, command=lambda:self.num_clicks('9')).grid(row=0, column=2)
+		self.button4 = ttk.Button(self.frame5,text='*',width=7,padding=7, command=lambda:self.calculate_method('*')).grid(row=0, column=3)
+
+		#frame6
+		self.button1 = ttk.Button(self.frame6,text='4',width=7,padding=7, command=lambda:self.num_clicks('4')).grid(row=0, column=0)
+		self.button2 = ttk.Button(self.frame6,text='5',width=7,padding=7, command=lambda:self.num_clicks('5')).grid(row=0, column=1)
+		self.button3 = ttk.Button(self.frame6,text='6',width=7,padding=7, command=lambda:self.num_clicks('6')).grid(row=0, column=2)
+		self.button4 = ttk.Button(self.frame6,text='/',width=7,padding=7, command=lambda:self.calculate_method('/')).grid(row=0, column=3)
+
+		#frame7
+		self.button1 = ttk.Button(self.frame7,text='1',width=7,padding=7, command=lambda:self.num_clicks('1')).grid(row=0, column=0)
+		self.button2 = ttk.Button(self.frame7,text='2',width=7,padding=7, command=lambda:self.num_clicks('2')).grid(row=0, column=1)
+		self.button3 = ttk.Button(self.frame7,text='3',width=7,padding=7, command=lambda:self.num_clicks('3')).grid(row=0, column=2)
+		self.button4 = ttk.Button(self.frame7,text='-',width=7,padding=7, command=lambda:self.calculate_method('-')).grid(row=0, column=3)
+
+		#frame8
+		self.button1 = ttk.Button(self.frame8,text='0',width=7,padding=7, command=lambda:self.num_clicks('0')).grid(row=0, column=0)
+		self.button2 = ttk.Button(self.frame8,text='.',width=7,padding=7, command=lambda:self.num_clicks('.')).grid(row=0, column=1)
+		self.button3 = ttk.Button(self.frame8,text='+',width=7,padding=7, command=lambda:self.calculate_method('+')).grid(row=0, column=2)
+		self.button4 = ttk.Button(self.frame8,text='=',width=7,padding=7, command=self.equals).grid(row=0, column=3)
+
+	def num_clicks(self,num):
+		#Delete any error message on the screen
+		if self.variable_check:
+			print('It is true')
+			self.display_window.delete('1.0','end')
+			self.variable_check = False # Return to false so as to append numbers to other numbers
+
+		self.num = num
+		self.display_window.insert(tk.INSERT,self.num) #Very dope line of code, just saying! doooooooppppeee
+
+	# def delete_num(self):
+	# 	self.display_window.delete(1.0)
+
+	def clear_screen(self):
+		self.display_window.delete('1.0','end')
+		#self.display_window.delete('1.0')
+
+	def constants_method(self, char_z):
+		self.char_z = char_z
+
+		if self.char_z == 'pi':
+			self.display_window.delete('1.0','end')			
+			self.display_window.insert(tk.INSERT,3.141592653589)
+
+
+			#print(self.num1_edit) #Check to see if it works
+			#print(self.char_x)
+			#print(self.num1_edit)
+
+			return
+
+	def calculate_method(self,char_x):
+		self.char_x = char_x
+		self.num1 = self.display_window.get('1.0',tk.END) #Check this line and understand it! 
+
+		#Remove whitespace and convert to int
+		for i in self.num1:
+			if i in punctuation or i == "\n":
+				self.num1_edit = self.num1.replace(i,'')
+		#print(self.num1_edit) #Check to see if it works
+
+		self.display_window.delete('1.0','end')
+
+
+		#self.display_window.insert(tk.INSERT,self.addition)
+
+	def equals(self):
+
+		self.num2 = self.display_window.get('1.0',tk.END) # Amazing Eurika moment
+
+		#Remove whitespace and convert
+		for i in self.num2:
+			if i in punctuation or i == "\n":
+				self.num2_edit = self.num2.replace(i,'')
+
+		#print(self.num2_edit) #Check to see if it works
+
+		#CHECK TO SEE WHAT OPERATION WAS CALLED
+
+		#ADDITION
+		if self.char_x == '+':
+			self.output_result = eval(self.num1_edit) + eval(self.num2_edit)
+
+		#SUBTRACTION
+		elif self.char_x == '-':
+			self.output_result = eval(self.num1_edit) - eval(self.num2_edit)
+
+		#DIVISION
+		elif self.char_x == '/':
+			# Exception Handling
+			if eval(self.num2_edit) == 0:
+				print('Zero as the second argument')
+
+				self.display_window.delete('1.0','end')
+				self.display_window.insert(tk.INSERT,self.error_message) #Insert the error message
+
+				# A variableto clear screen after a number has been clicked
+				self.variable_check = True
+
+				return
+
+			self.output_result = eval(self.num1_edit) / eval(self.num2_edit)
+
+		#MULTIPLICATION
+		elif self.char_x == '*':
+			self.output_result = eval(self.num1_edit) * eval(self.num2_edit)
+
+		#MULTIPLICATION
+		elif self.char_x == '%':
+			self.output_result = eval(self.num1_edit) % eval(self.num2_edit)
+
+		self.display_window.delete('1.0','end')
+		self.display_window.insert(tk.INSERT,self.output_result)
+
+
+
+if __name__ == "__main__":
+	app = App()
+	app.mainloop()
+
